36d8f393cd734ed23bb4a4e2cc8f85e9c41936dd
aki <64885025+aki-recapture@users.noreply.github.com>
2020-06-17 17:46:01 +0900

    Add files via upload

---
+++ MyMemoApp/MemoTableTableViewController.swift
@@ -0,0 +1,112 @@
+//
+//  MemoTableTableViewController.swift
+//  MyMemoApp
+//
+//  Created by 近藤明広 on 2020/06/16.
+//  Copyright © 2020 AkihiroKondo. All rights reserved.
+//
+
+import UIKit
+
+class MemoTableTableViewController: UITableViewController {
+    let userDefaults = UserDefaults.standard
+    
+//    var memos = ["blue","red","pink"]
+    var memos = [String()]
+    
+    @IBAction func unwindToMemoList(sender: UIStoryboardSegue)
+    {
+        guard let sourceVC = sender.source as?
+            MemoViewController, let memo = sourceVC.memo else{
+                return
+        }
+        if let selectedIndexPath = self.tableView.indexPathForSelectedRow {
+            self.memos[selectedIndexPath.row] = memo
+        } else {
+        self.memos.append(memo)
+        }
+        self.tableView.reloadData()
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.userDefaults.object(forKey: "memos") ! = nil do { self.memos = self.userDefaults.stringArray(forKey: "memos")!
+        }; else do {
+                self.memos = ["memo1","memo2","memo3"]
+        }
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.memos.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemoTableViewCell", for: indexPath)
+
+         //Configure the cell...
+        cell.textLabel?.text = self.memos[indexPath.row]
+        return cell
+    }
+//
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return "section-\(section)"
+//    }
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            self.memos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
